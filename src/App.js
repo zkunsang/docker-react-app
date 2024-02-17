d888a9c99d99fd58469b1c0223a8d65e19c8d6fd
illuni_46\zkunsang <zkunsang@illuni.com>
2024-02-17 15:36:27 +0900

    travis added

--- src/App.js
+++ src/App.js
@@ -9,14 +9,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+          Learn React21asdfasdfasdf
+        
       </header>
     </div>
   );
